04874bd388853937bddbab89310e5d18b0cef9de
Umair Bin Salman <umairbinsalmankhan@gmail.com>
2024-02-05 02:07:00 +0500

    Basic Uploader Completed
    Now can upload and view the uploaded files in sidebar.

--- client/src/app/pdf-viewer/pdf-viewer.component.ts
+++ client/src/app/pdf-viewer/pdf-viewer.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
+import { SidebarModule } from 'primeng/sidebar'
+import { ButtonModule } from 'primeng/button'
+import { FileUploadEvent, FileUploadModule } from 'primeng/fileupload'
 
 @Component({
   selector: 'app-pdf-viewer',
   standalone: true,
-  imports: [],
+  imports: [
+    PdfViewerModule,
+    SidebarModule,
+    ButtonModule,
+    FileUploadModule
+  ],
   templateUrl: './pdf-viewer.component.html',
   styleUrl: './pdf-viewer.component.css'
 })
 export class PdfViewerComponent {
+  isOpen: boolean = false;
+  filePaths: string[] = [];
 
+  async fileUploaded(event: FileUploadEvent) {
+    await event.files.map(file => {
+      this.filePaths.push(URL.createObjectURL(file));
+      
+    })
+  }
 }
